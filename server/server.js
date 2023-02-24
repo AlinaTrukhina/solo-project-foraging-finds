297cd95609f454f5770489e3853365d05d265d91
AlinaTrukhina <alina.trukhina@gmail.com>
2023-02-24 12:08:28 -0600

    update port

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use('/upload', uploadRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 /** Listen * */
 app.listen(PORT, () => {
